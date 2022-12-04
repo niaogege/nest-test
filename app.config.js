40ee19628a64847282ed55f552f623f130969b75
chendapeng <pengpeng.chen@ximalaya.com>
2022-12-04 20:56:33 +0800

    feat: main branch change path6

--- app.config.js
+++ app.config.js
@@ -11,11 +11,11 @@ module.exports = {
       watch: true,
       // 忽略掉一些目录文件的变化。
       // 由于把日志目录放到了项目路径下，一定要将其忽略，否则应用启动产生日志，pm2 监听到变化就会重启，重启又产生日志，就会进入死循环
-      ignore_watch: ['node_modules', 'logs'],
-      // 错误日志存放路径
-      err_file: path.resolve(__dirname, 'logs/error.log'),
-      // 打印日志存放路径
-      out_file: path.resolve(__dirname, 'logs/out.log'),
+      // ignore_watch: ['node_modules', 'logs'],
+      // // 错误日志存放路径
+      // err_file: path.resolve(__dirname, 'logs/error.log'),
+      // // 打印日志存放路径
+      // out_file: path.resolve(__dirname, 'logs/out.log'),
       // 设置日志文件中每条日志前面的日期格式
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
     },
